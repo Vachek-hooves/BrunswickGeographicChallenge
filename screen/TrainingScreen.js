c7c60bd5453c6f942584e869fc00b757b2796bfa
Vachek-hooves <vachek0602@gmail.com>
2024-08-19 17:58:20 +0300

    flatlist animation

--- screen/TrainingScreen.js
+++ screen/TrainingScreen.js
@@ -16,7 +16,7 @@ import {useRef} from 'react';
 const {width, height} = Dimensions.get('screen');
 const SPACING = 20;
 // 60- random size (but here it is element height, SPACING * 3)
-const ITEM_SIZE = 10 + SPACING * 3;
+const ITEM_SIZE = 10 + SPACING * 6;
 
 const TrainingScree = ({}) => {
   const {training} = useAppContext();
@@ -40,8 +40,18 @@ const TrainingScree = ({}) => {
             ITEM_SIZE * index,
             ITEM_SIZE * (index + 2),
           ];
+          const opacityInputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + 0.3),
+          ];
           const scale = scrollY.interpolate({
             inputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+          const opacity = scrollY.interpolate({
+            inputRange: opacityInputRange,
             outputRange: [1, 1, 1, 0],
           });
           return (
@@ -50,7 +60,7 @@ const TrainingScree = ({}) => {
                 style={{
                   // height: ITEM_SIZE,
                   padding: SPACING,
-                  marginBottom: SPACING,
+                  marginBottom: SPACING + 20,
                   borderRadius: 12,
                   borderColor: COLOR.teal,
                   backgroundColor: COLOR.purple + 90,
@@ -58,6 +68,7 @@ const TrainingScree = ({}) => {
                   shadowOffset: {width: 0, height: 10}, // Тінь тільки внизу
                   shadowOpacity: 0.5, // Відрегулюйте прозорість тіні за необхідністю
                   shadowRadius: 5,
+                  opacity,
                   transform: [{scale}],
                 }}>
                 <Text
